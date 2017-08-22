ca78f53c3355070444c93af32b24adf2bb0955e0
Dhruv Dogra <dhruv.mnit@gmail.com>
2017-08-21 23:26:10 -0400

    Tagging, comments and image implemented

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,5 +1,5 @@
 module RecipesHelper
   def recipe_params
-    params.require(:recipe).permit(:title, :body)
+    params.require(:recipe).permit(:title, :body, :tag_list, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820210526) do
+ActiveRecord::Schema.define(version: 20170822002903) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author_name"
@@ -28,6 +28,22 @@ ActiveRecord::Schema.define(version: 20170820210526) do
     t.text     "body"
     t.string   "ingredient"
     t.decimal  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["recipe_id"], name: "index_taggings_on_recipe_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
